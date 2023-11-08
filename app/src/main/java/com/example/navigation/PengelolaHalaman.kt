cc467c9c7c7258c16c586508ae296831895b1a14
MuhDavin213 <davin.new58@gmail.com>
2023-11-08 18:07:03 +0700

    Done

---
+++ app/src/main/java/com/example/navigation/PengelolaHalaman.kt
@@ -0,0 +1,124 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigation.data.SumberData.flavors
+
+enum class PengelolaHalaman {
+    Home,
+    Rasa,
+    Summary
+}
+
+@Composable
+fun EsJumboAppBar(
+    bisaNavigasiBack: Boolean,
+    navigasiUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(title = { Text(stringResource(id = R.string.app_name)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (bisaNavigasiBack) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.back_button)
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun EsJumboApp(
+    viewModel: OrderViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    Scaffold(
+        topBar = {
+            EsJumboAppBar(
+                bisaNavigasiBack = false,
+                navigasiUp = { /*TODO: implement back navigation*/
+                }
+            )
+        }
+    ) { innerPadding ->
+        val uiState by viewModel.stateUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = PengelolaHalaman.Home.name,
+            modifier = Modifier.padding(innerPadding)
+        )
+        {
+            composable(route = PengelolaHalaman.Home.name) {
+                HalamanHome(
+                    onNextButtonClicked = {
+                        navController.navigate(PengelolaHalaman.Rasa.name)
+                    })
+                val context = LocalContext.current
+                HalamanSatu(
+                    pilihanRasa = flavors.map { id ->
+                        context.resources.getString(id)
+                    },
+                    onSelectionChanged = { viewModel.setRasa(it) },
+                    onConfirmButtonClicked = {
+                        viewModel.setJumlah(it)
+                    },
+                    onNextButtonClicked = {
+                        navController.navigate(PengelolaHalaman.Summary.name)
+                    },
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateHome(
+                            viewModel,
+                            navController
+                        )
+                    })
+            }
+            composable(route = PengelolaHalaman.Summary.name) {
+                HalamanDua(
+                    orderUIState = uiState,
+                    onCancelButtonClicked = { cancelOrderAndNavigateToRasa(navController) })
+            }
+        }
+    }
+}
+
+private fun cancelOrderAndNavigateHome(
+    viewModel: OrderViewModel,
+    navController: NavHostController
+) {
+    viewModel.resetOrder()
+    navController.popBackStack(PengelolaHalaman.Home.name, inclusive = false)
+}
+
+fun cancelOrderAndNavigateToRasa(
+    navController: NavHostController
+) {
+    navController.popBackStack(PengelolaHalaman.Rasa.name, inclusive = false)
+}
