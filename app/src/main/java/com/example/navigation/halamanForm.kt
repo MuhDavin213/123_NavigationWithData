cb936aa971031e22decf2b6658a72b4d04c4aa26
MuhDavin213 <davin.new58@gmail.com>
2023-11-15 18:09:17 +0700

    menambahkan function baru dan variable dengan mutablestateof

--- app/src/main/java/com/example/navigation/halamanForm.kt
+++ app/src/main/java/com/example/navigation/halamanForm.kt
@@ -1,2 +1,24 @@
 package com.example.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun HalamanBaru(
+  goToNextPage: (MutableList<String>) -> Unit,
+){
+    var namaTxt by remember {
+        mutableStateOf("")
+    }
+    var telpTxt by remember {
+        mutableStateOf("")
+    }
+    var alamatTxt by remember {
+        mutableStateOf("")
+    }
+    val listData = mutableListOf<String>(namaTxt,telpTxt,alamatTxt)
+
+}
