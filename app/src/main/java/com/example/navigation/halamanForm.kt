8fa6099769a7cf5e2e5575379b1020cb6d14aebe
MuhDavin213 <davin.new58@gmail.com>
2023-11-16 10:32:16 +0700

    Mengatur Layout dengan Column dan layout lainnya

--- app/src/main/java/com/example/navigation/halamanForm.kt
+++ app/src/main/java/com/example/navigation/halamanForm.kt
@@ -1,11 +1,19 @@
 package com.example.navigation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HalamanBaru(
@@ -24,7 +32,12 @@ fun HalamanBaru(
 
 
     Column (
-
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .background(color = Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
 
     }
