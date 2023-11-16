e735b7e872ed9a67d5fbf0abec74e9592e02e770
MuhDavin213 <davin.new58@gmail.com>
2023-11-16 10:43:57 +0700

    add form pelanggan dengan label

--- app/src/main/java/com/example/navigation/halamanForm.kt
+++ app/src/main/java/com/example/navigation/halamanForm.kt
@@ -4,7 +4,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,6 +22,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanBaru(
   goToNextPage: (MutableList<String>) -> Unit,
@@ -47,6 +52,27 @@ fun HalamanBaru(
             modifier = Modifier.padding(20.dp),
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold
+        )
+        OutlinedTextField(
+            value = namaTxt,
+            onValueChange = {
+                namaTxt = it
+            },
+            label = { Text(text = "Nama")}
+        )
+        OutlinedTextField(
+            value = telpTxt,
+            onValueChange = {
+                telpTxt = it
+            },
+            label = { Text(text = "No Telepon")}
+        )
+        OutlinedTextField(
+            value = alamatTxt,
+            onValueChange = {
+                alamatTxt = it
+            },
+            label = { Text(text = "Alamat")}
         )
 
     }
