de8ade06af8bec2cbb6c32dd22e74cd12bcacf73
MuhDavin213 <davin.new58@gmail.com>
2023-11-16 10:35:41 +0700

    add text pada halaman pelanggan

--- app/src/main/java/com/example/navigation/halamanForm.kt
+++ app/src/main/java/com/example/navigation/halamanForm.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,7 +14,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HalamanBaru(
@@ -39,6 +42,12 @@ fun HalamanBaru(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Text(
+            text = "Data Pelanggan",
+            modifier = Modifier.padding(20.dp),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
 
     }
 }
